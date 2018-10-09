858636b76bbe76a6a66c800bb02587a10a82d03d
Vyacheslav <zlav24va@mail.ru>
2018-10-09 14:41:47 +0300

    Update index.js

--- client/src/components/User/index.js
+++ client/src/components/User/index.js
@@ -1,69 +1,75 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+
+import './styles.css'
 
 export class User extends Component {
   logoutHandler = async () => {
-    const response = await fetch('/api/users/logout', {
-      method: 'POST',
+    const response = await fetch("/api/users/logout", {
+      method: "POST"
     })
     const json = await response.json()
     console.log(json)
   }
   deleteAllCookies = () => {
-    var cookies = document.cookie.split('')
+    var cookies = document.cookie.split("")
     console.log(cookies)
-    for (var i = 0; i < cookies.length; i++) {
+    for (var i = 0 i < cookies.length i++) {
       var cookie = cookies[i]
-      var eqPos = cookie.indexOf('=')
+      var eqPos = cookie.indexOf("=")
       var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie
-      document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT'
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT"
     }
-    localStorage.removeItem('username')
-    console.log('delete cookies')
+    localStorage.removeItem("username")
+    console.log("delete cookies")
     this.logoutHandler()
-    window.location = '/'
+    window.location = "/"
   }
   handleSubmit = e => {
     e.preventDefault()
     console.log(e.target.elements)
-    const login = e.target.elements['login'].value
-    const password = e.target.elements['password'].value
+    const login = e.target.elements["login"].value
+    const password = e.target.elements["password"].value
     this.props.handleLogin({ login: login, password: password })
   }
   renderTemplate = () => {
     const { name, error, isFetching } = this.props
 
     if (error) {
-      return <p>Во время запроса произошла ошибка, обновите страницу</p>
+      return (
+        <p>
+          Auth error. Please try again.
+        </p>
+      )
     }
 
     if (isFetching) {
-      return <p>Загружаю...</p>
+      return <p>Loading...</p>
     }
 
     if (name) {
       return (
-        <div>
-          <p>Привет, {name}!</p>
-          <button className="btn" onClick={this.deleteAllCookies}>
-            Выйти
-          </button>
-        </div>
+        <header>
+          <div className="user">Hi, {name}!</div>
+          <button onClick={this.deleteAllCookies}>Exit</button>
+        </header>
       )
     } else {
       return (
-        <form className="col-md-4" onSubmit={this.handleSubmit}>
-          <input type="text" name="login" placeholder="login" />
-          <input type="password" name="password" placeholder="password" />
-          <button type="submit" className="btn">
-            Войти
-          </button>
-        </form>
+        <div className="login-page">
+          <div className="form">
+            <form className="login-form" onSubmit={this.handleSubmit}>
+              <input type="text" name="login" placeholder="login" />
+              <input type="password" name="password" placeholder="password" />
+              <button type="submit">Login</button>
+            </form>
+          </div>
+        </div>
       )
     }
   }
   render() {
-    return <div className="ib user">{this.renderTemplate()}</div>
+    return this.renderTemplate() //<div className="ib user"></div>
   }
 }
 
@@ -71,5 +77,5 @@ User.propTypes = {
   name: PropTypes.string.isRequired,
   error: PropTypes.string,
   isFetching: PropTypes.bool.isRequired,
-  handleLogin: PropTypes.func.isRequired,
+  handleLogin: PropTypes.func.isRequired
 }
