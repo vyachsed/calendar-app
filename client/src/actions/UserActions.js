2aa6c0710d73a0dd4d45c7c3ce68f0ccf238716f
Vyacheslav <zlav24va@mail.ru>
2018-10-09 14:26:43 +0300

    Update UserActions.js

--- client/src/actions/UserActions.js
+++ client/src/actions/UserActions.js
@@ -2,7 +2,7 @@ export const LOGIN_REQUEST = 'LOGIN_REQUEST'
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
 export const LOGIN_FAIL = 'LOGIN_FAIL'
 
-export function handleLogin(callback, user) {
+export async function handleLogin(callback, user) {
   return function(dispatch) {
     dispatch({
       type: LOGIN_REQUEST,
@@ -20,7 +20,7 @@ export function handleLogin(callback, user) {
       return false
     }
 
-    if (request()) {
+    if (await request()) {
       let username = user.login
       localStorage.setItem('username', username)
 
