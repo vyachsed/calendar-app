760e5dbb717fef2fa27495fb8c9be3d315c47700
Vyacheslav <zlav24va@mail.ru>
2018-10-14 17:37:13 +0300

    Update App.js

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -3,16 +3,6 @@ import UserContainer from '../../containers/UserContainer'
 import MyCalendarContainer from '../../containers/MyCalendarContainer'
 
 class App extends Component {
-  state = { status: false }
-  checkAuth = async () => {
-    const response = await fetch('/api/users/me', { method: 'GET' })
-    const json = await response.json()
-    console.log(json.status)
-    if (json.status === 'ok') {
-      return true
-    }
-    return false
-  }
   render() {
     return (
       <div className="app">
